051ad0abea118e2236b8c8df67478baf68d7f83b
Vibha <vibha.srv@gmail.com>
2020-09-30 12:51:20 +0530

    test

--- src/main/java/ImplicitExplicitFluentWait/OpenChrome.java
+++ src/main/java/ImplicitExplicitFluentWait/OpenChrome.java
@@ -10,29 +10,31 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class OpenChrome {
 
-	public static void main(String[] args) {
+	@Test
+	public void OpenChromeMethod() {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.facebook.com/");
-		
+
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-		
+
 		WebDriverWait wait = new WebDriverWait(driver, 20);
-		
-		WebElement UserName =driver.findElement(By.id("email"));
+
+		WebElement UserName = driver.findElement(By.id("email"));
 		UserName.sendKeys("vibha.srv@gmail.com");
 		WebElement Password = driver.findElement(By.id("pass"));
 		Password.sendKeys("ajjayya09*");
-		//wait.until(ExpectedConditions.presenceOfElementLocated(By.id("u_0_b"))).click();;
-		driver.findElement(By.id("u_0_b")).click();	
-		
-		//Extra line added to verify in GIT
+		// wait.until(ExpectedConditions.presenceOfElementLocated(By.id("u_0_b"))).click();;
+		driver.findElement(By.id("u_0_b")).click();
+
+		// Extra line added to verify in GIT
 	}
 
 }
